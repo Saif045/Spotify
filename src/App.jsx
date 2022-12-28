bbdb1a8a8d40fb531b232cbe47286f2323a8c1cb
Saif045 <saif.x45@gmail.com>
2022-12-28 23:56:13 +0200

    fsaf

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   const { activeSong } = useSelector((state) => state.player);
 
   return (
-    <div className="relative flex h-screen">
+    <div className="relative flex h-screen w-screen">
       <ScrollToTop />
       <Sidebar />
 
@@ -27,7 +27,7 @@ const App = () => {
       </Routes>
 
       {activeSong?.title && (
-        <div className="w-screen absolute z-[100] h-16 sm:h-20 bottom-12 sm:bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#022008] backdrop-blur-lg rounded-t-3xl z-100">
+        <div className=" hide-overflow w-screen absolute z-[100] h-16 sm:h-24 bottom-12 sm:bottom-0  flex animate-slideup bg-gradient-to-br from-white/10 to-[#022008] backdrop-blur-lg rounded-t-3xl z-100">
           <MusicPlayer />
         </div>
       )}
